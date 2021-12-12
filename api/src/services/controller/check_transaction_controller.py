e6bb6607ae4a6ab9d0a305f3ce6ce5983f6e53ae
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-12 15:00:18 -0300

    refactor: adjusment on controller to call get_machine_learning_factory for inject in usecase

--- api/src/services/controller/check_transaction_controller.py
+++ api/src/services/controller/check_transaction_controller.py
@@ -1,16 +1,15 @@
 from traceback import format_exc
 
-from src.services.common.machine_learning.machine_learning import MachineLearning
 from src.services.enum.http_status_enum import HTTPResponseStatus
 from src.services.usecase.check_transaction.check_transaction_usecase import CheckTransactionUsecase
 from src.services.contracts.controller_contract import HttpRequest, HttpResponse
-
+from src.main.factories.get_machine_learning_factory import get_machine_learning_factory
 
 class CheckTransactionController:
     
     def check_transaction(self, http_request: HttpRequest) -> HttpResponse:
         try:
-            response = CheckTransactionUsecase(MachineLearning()).execute(http_request.payload)
+            response = CheckTransactionUsecase(get_machine_learning_factory()).execute(http_request.payload)
             return HttpResponse(HTTPResponseStatus.SUCCESS, response)
 
         except Exception:
