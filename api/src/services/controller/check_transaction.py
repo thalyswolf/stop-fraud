fbfa27e6472c3e18fc9ca482e9cf222f87eaa125
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-12 14:38:03 -0300

    refactor: adjustment on controller

--- api/src/services/controller/check_transaction.py
+++ api/src/services/controller/check_transaction.py
@@ -1,3 +1,7 @@
+from traceback import format_exc
+
+from src.services.common.machine_learning.machine_learning import MachineLearning
+from src.services.enum.http_status_enum import HTTPResponseStatus
 from src.services.usecase.check_transaction.check_transaction_usecase import CheckTransactionUsecase
 from src.services.contracts.controller_contract import HttpRequest, HttpResponse
 
@@ -5,4 +9,11 @@ from src.services.contracts.controller_contract import HttpRequest, HttpResponse
 class CheckTransactionController:
     
     def check_transaction(self, http_request: HttpRequest) -> HttpResponse:
-        response = CheckTransactionUsecase().execute(http_request.payload)
+        try:
+            response = CheckTransactionUsecase(MachineLearning()).execute(http_request.payload)
+            return HttpResponse(HTTPResponseStatus.SUCCESS, response)
+
+        except Exception:
+            return HttpResponse(HTTPResponseStatus.ERROR, {
+                'message': format_exc()
+            })
