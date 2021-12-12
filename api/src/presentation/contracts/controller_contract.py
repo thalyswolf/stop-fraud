4afbb5719f15df338f99b15e4c1f1567f75bf0eb
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-12 13:07:21 -0300

    chore: add controller contract

---
+++ api/src/presentation/contracts/controller_contract.py
@@ -0,0 +1,18 @@
+class HttpRequest:
+    def __init__(self, header, payload, params):
+        self.header = header
+        self.payload = payload
+        self.params = params
+    def __repr__(self):
+        return (
+            f"HttpRequest (header={self.header}, body={self.body}, query={self.query})"
+        )
+
+class HttpResponse:
+
+    def __init__(self, status_code: int, body: any):
+        self.status_code = status_code
+        self.body = body
+
+    def __repr__(self):
+        return f"HttpResponse (status_code={self.status_code}, body={self.body})"
