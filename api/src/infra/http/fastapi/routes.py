8c9d8677ad8f70359d1a550a8f6bed720b9a449b
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-12 20:47:08 -0300

    refactor: adjustment controller import

--- api/src/infra/http/fastapi/routes.py
+++ api/src/infra/http/fastapi/routes.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Response
 
 from src.domain.usecase.check_transaction import CheckTransactionRequest, CheckTransactionResponse
 from src.main.adapters.http.fast_api_adapter import fast_api_adapter
-from src.services.controller.check_transaction import CheckTransactionController
+from src.services.controller.check_transaction_controller import CheckTransactionController
 
 app = FastAPI()
 
