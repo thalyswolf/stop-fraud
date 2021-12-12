43a58c9e2c744fe72e1f5e93a5b362aa14fa85c4
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-12 13:07:03 -0300

    chore: add fastapi adapter

---
+++ api/src/main/adapters/http/fast_api_adapter.py
@@ -0,0 +1,13 @@
+from typing import Dict
+
+from src.presentation.contracts.controller_contract import HttpRequest
+
+
+def fast_api_adapter(request: Dict) -> HttpRequest:
+    http_request = HttpRequest(
+        header=request['headers'],
+        payload=request['body'],
+        params=request['query']
+    )
+
+    return http_request
