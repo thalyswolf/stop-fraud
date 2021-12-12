634e156c0cf3110c9b6c7cc0d397b6cf4afcccf2
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-12 14:38:34 -0300

    refactor: add predict on usecase

--- api/src/services/usecase/check_transaction/check_transaction_usecase.py
+++ api/src/services/usecase/check_transaction/check_transaction_usecase.py
@@ -1,7 +1,27 @@
 from typing import Dict
 
+from src.domain.entities.transaction import Transaction
+from src.domain.usecase.check_transaction import CheckTransactionRequest
+from src.services.contracts.machine_learning_contract import MachineLearningContract
 
 class CheckTransactionUsecase:
     
-    def execute(self, request: Dict) -> Dict:
-        print('chegou no usecase')
+    def __init__(self, machine_learning: MachineLearningContract) -> None:
+        self._machine_learning = machine_learning
+
+    def execute(self, request: CheckTransactionRequest) -> Dict:
+
+        transaction = Transaction()
+        transaction.transaction_id = request.transaction_id
+        transaction.user_id = request.user_id
+        transaction.card_number = request.card_number 
+        transaction.transaction_date = request.transaction_date
+        transaction.transaction_amount = request.transaction_amount
+        transaction.device_id = request.device_id
+
+        predict = self._machine_learning.predict(transaction)
+
+        return {
+            'transaction_id': transaction.transaction_id,
+            'recommendation': predict.status
+        }
