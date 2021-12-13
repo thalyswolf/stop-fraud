d135bb07f291de125ac358652d2ccf35f10a01bc
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-12 22:50:21 -0300

    refactor: adjustment for handle errors on controller

--- api/src/services/controller/check_transaction_controller.py
+++ api/src/services/controller/check_transaction_controller.py
@@ -4,6 +4,7 @@ from src.services.enum.http_status_enum import HTTPResponseStatus
 from src.services.usecase.check_transaction.check_transaction_usecase import CheckTransactionUsecase
 from src.services.contracts.controller_contract import HttpRequest, HttpResponse
 from src.main.factories.get_machine_learning_factory import get_machine_learning_factory
+from src.services.errors.handler import InvalidAmountErrorException
 
 class CheckTransactionController:
     
@@ -12,7 +13,12 @@ class CheckTransactionController:
             response = CheckTransactionUsecase(get_machine_learning_factory()).execute(http_request.payload)
             return HttpResponse(HTTPResponseStatus.SUCCESS, response)
 
+        except InvalidAmountErrorException as ie:
+            return HttpResponse(HTTPResponseStatus.INVALID_DATA, {
+                'error': ie.message
+            })
+
         except Exception:
             return HttpResponse(HTTPResponseStatus.ERROR, {
-                'message': format_exc()
+                'error': format_exc()
             })
