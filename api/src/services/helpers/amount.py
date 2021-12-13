dfc2d9a6a6045e062360395dd70f23da2c65674d
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-12 23:04:08 -0300

    refactor: convertion

--- api/src/services/helpers/amount.py
+++ api/src/services/helpers/amount.py
@@ -1,6 +1,6 @@
-def format_amount_from_int_to_float(amount: int):
+def format_amount_from_int_to_float(amount: int) -> float:
     return float(amount) / 100.0
 
-def format_amount_from_float_to_int(amount: float):
+def format_amount_from_float_to_int(amount: float) -> int:
     amount = "%.2f" % float(amount)
     return int(str(amount.replace(".", "").replace(",", "")))
