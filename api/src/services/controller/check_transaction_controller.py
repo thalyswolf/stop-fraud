355ece4830ed97ca0655f02b96cc5ddbd9c9d6a8
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-13 22:11:27 -0300

    refactor: injection transaction repository in usecase

--- api/src/services/controller/check_transaction_controller.py
+++ api/src/services/controller/check_transaction_controller.py
@@ -4,13 +4,19 @@ from src.services.enum.http_status_enum import HTTPResponseStatus
 from src.services.usecase.check_transaction.check_transaction_usecase import CheckTransactionUsecase
 from src.services.contracts.controller_contract import HttpRequest, HttpResponse
 from src.main.factories.get_machine_learning_factory import get_machine_learning_factory
+from src.main.factories.get_transaction_repository_factory import get_transaction_repository_factory
 from src.services.errors.handler import InvalidAmountErrorException
 
 class CheckTransactionController:
     
     def check_transaction(self, http_request: HttpRequest) -> HttpResponse:
         try:
-            response = CheckTransactionUsecase(get_machine_learning_factory()).execute(http_request.payload)
+            machine_learning = get_machine_learning_factory()
+            transaction_repository = get_transaction_repository_factory()
+
+            response = CheckTransactionUsecase(machine_learning, transaction_repository)\
+                                            .execute(http_request.payload)
+
             return HttpResponse(HTTPResponseStatus.SUCCESS, response)
 
         except InvalidAmountErrorException as ie:
