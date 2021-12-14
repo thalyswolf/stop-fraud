fe3abcd3d7f6866c0630a54c4eeee4471e6fa5de
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-14 13:21:36 -0300

    chore: model mongo repository

---
+++ api/src/infra/database/repositories/model_repository_mongo.py
@@ -0,0 +1,17 @@
+import pymongo
+
+from src.services.contracts.model_repository_contract import ModelRepositoryContract
+from src.infra.database.mongo.connection import MongoConnection
+from src.domain.entities.learning_model import LearningModel
+from src.main.adapters.database.mongo_to_learning_model_adapter import mongo_to_learning_model_adapter
+
+class ModelRepositoryMongo(ModelRepositoryContract):
+
+    def __init__(self) -> None:
+        database = MongoConnection().get_database()
+        self.collection_name = database['models']
+
+
+    def get_last_learned_model(self) -> LearningModel:
+        result = self.collection_name.find().sort("createdAt", pymongo.DESCENDING)
+        return mongo_to_learning_model_adapter(result[0])
