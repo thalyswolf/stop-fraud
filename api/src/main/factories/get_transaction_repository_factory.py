500603b81fd9beaaad4bc4eeae9b01ca79e11612
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-13 22:50:40 -0300

    refactor: adjustment to call mongo repository in factory

--- api/src/main/factories/get_transaction_repository_factory.py
+++ api/src/main/factories/get_transaction_repository_factory.py
@@ -1,4 +1,4 @@
-from src.infra.database.repositories.transaction_repository_test import TransactionRepositoryTest
+from src.infra.database.repositories.transaction_repository_mongo import TransactionRepositoryMongo
 
 def get_transaction_repository_factory():
-    return TransactionRepositoryTest()
+    return TransactionRepositoryMongo()
