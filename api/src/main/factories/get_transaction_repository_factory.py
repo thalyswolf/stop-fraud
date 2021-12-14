3208feec7b650a18955493f7eda031e50aebb8ea
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-13 22:10:59 -0300

    chore: add transaction repository factory

---
+++ api/src/main/factories/get_transaction_repository_factory.py
@@ -0,0 +1,4 @@
+from src.infra.database.repositories.transaction_repository_test import TransactionRepositoryTest
+
+def get_transaction_repository_factory():
+    return TransactionRepositoryTest()
