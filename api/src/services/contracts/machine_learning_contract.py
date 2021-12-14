773d9cafa4ce4549e67e8f7349d18e4ea323f97f
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-14 13:24:04 -0300

    refactor: adjustment on machine learning contract

--- api/src/services/contracts/machine_learning_contract.py
+++ api/src/services/contracts/machine_learning_contract.py
@@ -3,10 +3,10 @@ from typing import Dict, List
 
 from src.domain.entities.predict import Predict
 from src.domain.entities.transaction import Transaction
-
+from src.domain.entities.learning_model import LearningModel
 
 class MachineLearningContract(ABC):
 
     @abstractmethod
-    def predict(self, transaction: Transaction) -> Predict:
+    def predict(self, learning_model: LearningModel, transaction: Transaction) -> Predict:
         pass
