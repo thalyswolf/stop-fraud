0e0e7caf22b35e211175822d6378aa57c832db90
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-15 23:15:06 -0300

    refactor: adjustment

--- api/src/main/adapters/common/machile_learning_decode_adapter.py
+++ api/src/main/adapters/common/machile_learning_decode_adapter.py
@@ -1,11 +1,9 @@
 from typing import List
 from src.domain.entities.predict import Predict
-
-PREDICT_STATUS_APPROVE = 'approve'
-PREDICT_STATUS_DENY = 'deny'
+from src.services.enum.recommendation_enum import APPROVE, DENY
 
 
 def machine_learning_decode_adapter(predict_response: List[bool]) -> Predict:
     predict = Predict()
-    predict.status = PREDICT_STATUS_APPROVE if not predict_response[0] else PREDICT_STATUS_DENY
+    predict.status = APPROVE if not predict_response[0] else DENY
     return predict
