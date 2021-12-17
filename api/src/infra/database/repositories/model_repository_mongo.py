4948128400d636450438798323cc40b68d2553a1
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-17 11:34:10 -0300

    refactor: add handle 404 on repository

--- api/src/infra/database/repositories/model_repository_mongo.py
+++ api/src/infra/database/repositories/model_repository_mongo.py
@@ -4,6 +4,7 @@ from src.services.contracts.model_repository_contract import ModelRepositoryCont
 from src.infra.database.mongo.connection import MongoConnection
 from src.domain.entities.learning_model import LearningModel
 from src.main.adapters.database.mongo_to_learning_model_adapter import mongo_to_learning_model_adapter
+from src.services.errors.handler import NotFoundDBErrorException
 
 class ModelRepositoryMongo(ModelRepositoryContract):
 
@@ -14,4 +15,9 @@ class ModelRepositoryMongo(ModelRepositoryContract):
 
     def get_last_learned_model(self) -> LearningModel:
         result = self.collection_name.find().sort("createdAt", pymongo.DESCENDING)
+
+        if result.count() == 0:
+            raise NotFoundDBErrorException()
+
         return mongo_to_learning_model_adapter(result[0])
+                   
